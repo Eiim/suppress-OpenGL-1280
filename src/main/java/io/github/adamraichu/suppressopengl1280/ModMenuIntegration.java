12e9281aceb772dd3b6486e200dec67f0b0b9d44
Ethan Chapman <ethan@we3chapmans.com>
2026-05-22 14:56:17 -0400

    Fix Mod Menu integration

--- src/main/java/io/github/adamraichu/suppressopengl1280/ModMenuIntegration.java
+++ src/main/java/io/github/adamraichu/suppressopengl1280/ModMenuIntegration.java
@@ -4,11 +4,11 @@ import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
 import io.github.adamraichu.suppressopengl1280.config.ConfigOptions;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ConfigOptions.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ConfigOptions.class, parent).get();
     }
 }
